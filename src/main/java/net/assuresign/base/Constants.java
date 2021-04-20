d38f3db47610824766844dea2d774fb1eb6c4899
Tushar Kanta Behera <tushar.kec143@gmail.com>
2021-04-20 16:00:03 +0530

    mail

--- src/main/java/net/assuresign/base/Constants.java
+++ src/main/java/net/assuresign/base/Constants.java
@@ -2,9 +2,9 @@ package net.assuresign.base;
 
 public class Constants {
 
-	public static final String ENV = "qa-test";
+	public static final String ENV = "dev";
 
-	public static  double START_VERSION = 3.0;
+	public static  double START_VERSION = 3.7;
 	public static  double END_VERSION = 3.7;
 
 	public static  String EMAIL_ACCOUNT_ID;;
