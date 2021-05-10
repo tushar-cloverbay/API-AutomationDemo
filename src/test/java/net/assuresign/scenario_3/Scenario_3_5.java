9985e52db95dcc51fa09d422b22cb070134266d9
Tushar Kanta Behera <tushar.kec143@gmail.com>
2021-05-10 12:10:34 +0530

    Addition of old apis

--- src/test/java/net/assuresign/scenario_3/Scenario_3_5.java
+++ src/test/java/net/assuresign/scenario_3/Scenario_3_5.java
@@ -24,8 +24,14 @@ public class Scenario_3_5 extends Base {
 		String token = TestUtils.getToken(version);
 		String URI = "https://" + Constants.ENV + ".assuresign.net/api/documentnow/v" + version + "/submit";
 		extentTest.log(LogStatus.PASS, "API URI : " + URI);
-		String payload = JsonUtils.payloadGenerator(
-				"Inputs\\" + Constants.ENV + "\\Scenario_3\\submit_jotBlockParsingWithPageCount.json");
+		String payload;
+		if(version.equals("3.0")||version.equals("3.1")||version.equals("3.2"))
+		{
+			payload = JsonUtils.payloadGenerator("Inputs\\"+Constants.ENV+"\\Scenario_3\\submit_jotBlockParsingWithPageCount-"+version+".json");
+		}else
+		{
+			payload = JsonUtils.payloadGenerator("Inputs\\"+Constants.ENV+"\\Scenario_3\\submit_jotBlockParsingWithPageCount.json");
+		}
 		RequestSpecification request = RestAssured.given().header("Authorization", "Bearer " + token).body(payload);
 		request.header("Content-Type", "application/json");
 		Response response = request.post(URI);
@@ -33,10 +39,21 @@ public class Scenario_3_5 extends Base {
 		extentTest.log(LogStatus.PASS, "Response Time : " + response.getTime() + " milliseconds");
 		System.out.println(response.getBody().asString());
 		System.out.println(response.getStatusCode());
-		response.then().assertThat().statusCode(equalTo(200))
-		.body("$", hasKey("messages"))
-		.body("result.envelopeID", notNullValue())
-		.body("result.authToken", notNullValue());
+		if(version.equals("3.0")||version.equals("3.1"))
+		{
+			response.then().assertThat()
+			.statusCode(equalTo(200))
+			.body("$", hasKey("messages"))
+			.body("result.id", notNullValue())
+			.body("result.authToken", notNullValue());
+		}else
+		{
+			response.then().assertThat()
+			.statusCode(equalTo(200))
+			.body("$", hasKey("messages"))
+			.body("result.envelopeID", notNullValue())
+			.body("result.authToken", notNullValue());
+		}
 	}
 
 	@Test(dataProvider = "version-data-provider", enabled = true)
