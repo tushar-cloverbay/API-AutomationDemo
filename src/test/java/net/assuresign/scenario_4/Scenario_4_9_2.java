6c24f2386d5e048f8699c01ea93fecebf8acf504
Tushar Kanta Behera <tushar.kec143@gmail.com>
2021-05-28 13:36:29 +0530

    Added Scenario 4.9.1

---
+++ src/test/java/net/assuresign/scenario_4/Scenario_4_9_2.java
@@ -0,0 +1,52 @@
+package net.assuresign.scenario_4;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import net.assuresign.base.Base;
+import net.assuresign.base.Constants;
+import net.assuresign.utils.JsonUtils;
+import net.assuresign.utils.TestUtils;
+
+public class Scenario_4_9_2 extends Base{
+	@Test(dataProvider = "version-data-provider",groups = { "ExcludeFor3.0" },enabled = true)
+	public void submitPrepare_DynamicJotBlockParsing_SpecifiedSchema(String version) throws IOException {
+		extentTest.log(LogStatus.PASS, "Test Description : " + "Scenario_4_9 : Test for getting Prepared Enveloped ID with Dynamic Jot Block Parsing");
+		apiVersion = version;
+		String token =TestUtils.getToken(version);
+		String URI = "https://"+Constants.ENV+".assuresign.net/api/documentnow/v"+ version +"/submit/prepare";
+		extentTest.log(LogStatus.PASS, "API URI : " + URI);
+		String payload;
+		if(version.equals("3.0")||version.equals("3.1")||version.equals("3.2"))
+		{
+			payload = JsonUtils.payloadGenerator("Inputs\\"+Constants.ENV+"\\Scenario_4\\preparedEID-DynamicJotParsing-"+version+".json");
+		}else
+		{
+			payload = JsonUtils.payloadGenerator("Inputs\\"+Constants.ENV+"\\Scenario_4\\preparedEID-DynamicJotParsing.json");
+		}
+		RequestSpecification request = RestAssured.given().header("Authorization", "Bearer "+token).body(payload);
+		request.header("Content-Type", "application/json");
+		Response response = request.post(URI);
+		responseBody = response.asPrettyString();
+		statusCode = Integer.toString(response.getStatusCode());
+		extentTest.log(LogStatus.PASS, "Response Time : " + response.getTime() +" milliseconds");
+		System.out.println(response.getBody().asString());
+		if (version.equals("3.0") || version.equals("3.1")) {
+			response.then().assertThat().statusCode(equalTo(200)).body("$", hasKey("messages"))
+					.body("result.preparedEnvelopeId", notNullValue());
+		} else {
+			response.then().assertThat().statusCode(equalTo(200)).body("$", hasKey("messages"))
+					.body("result.preparedEnvelopeID", notNullValue());
+		}
+	}
+}
